6ac527bc323c8b0a5d64feb5e556d0c1e31a7d83
Nnamdi Ajoku <90708381+ajaycmput@users.noreply.github.com>
2022-07-22 02:28:56 -0600

    updated .php file, created and styled new redirect page

---
+++ mail.php
@@ -0,0 +1,21 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$email_subject= $_POST['message-subject']
+$message= $_POST['message'];
+$to = "ajaydrippin@gmail.com";
+
+$subject = "someone emailed you from your website!";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Header =" . $email_subject ."
+    \r\n Message =" .$message;
+
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:redirect.html");
+?>
