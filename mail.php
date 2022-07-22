512d9c64966714456c766d475d33774ee2fc7577
Nnamdi Ajoku <90708381+ajaycmput@users.noreply.github.com>
2022-07-22 03:47:26 -0600

    php-fix

--- mail.php
+++ mail.php
@@ -1,21 +1,20 @@
 <?php
 //get data from form  
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email= $_POST['emailaddress'];
+    $email_subject= $_POST['message-subject']
+    $message= $_POST['message'];
+    $to = "ajaydrippin@gmail.com";
 
-$name = $_POST['name'];
-$email= $_POST['emailaddress'];
-$email_subject= $_POST['message-subject']
-$message= $_POST['message'];
-$to = "ajaydrippin@gmail.com";
-
-$subject = "someone emailed you from your website!";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Header =" . $email_subject ."
+    $subject = "someone emailed you from your website!";
+    $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Header =" . $email_subject ."
     \r\n Message =" .$message;
 
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
+    $headers = "From: ".$email;
+
     mail($to,$subject,$txt,$headers);
-    header("Location:redirect.html");
-}
-//redirect
+    header("Location: redirect.html");
+    }
+
 ?>
