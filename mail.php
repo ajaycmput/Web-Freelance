f170910bc1b25e841ee60fa5afb97e96ecdbab28
Nnamdi Ajoku <90708381+ajaycmput@users.noreply.github.com>
2022-07-31 04:31:43 -0600

    php-fix

--- mail.php
+++ mail.php
@@ -1,17 +1,17 @@
 <?php 
-    $name = $_POST['name'];
-    $email= $_POST['emailaddress'];
-    $email_subject= $_POST['message-subject']
-    $message= $_POST['message'];
-    $to = "ajaydrippin@gmail.com";
+$name = $_POST['name'];
+$email= $_POST['emailaddress'];
+$email_subject= $_POST['message-subject']
+$message= $_POST['message'];
+$to = "ajaydrippin@gmail.com";
 
-    $subject = "someone emailed you from your website!";
-    $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Header =" . $email_subject ."
-    \r\n Message =" .$message;
+$subject = "someone emailed you from your website!";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Header =" . $email_subject ."
+\r\n Message =" .$message;
 
-    $headers = "From: ".$email;
+$headers = "From: ".$email;
 
-    mail($to, $subject, $txt, $headers);
-    header("Location: redirect.html");
+mail($to, $subject, $txt, $headers);
+header("Location: redirect.html");
 
 ?> 
